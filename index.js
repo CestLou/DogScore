84b9c17da2a0c8917ebe137e40678c2468d8a2ae
CestLou <alisonrebeccarobb@gmail.com>
2014-12-13 19:05:41 -0800

    Work on CSS

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+//all keys have a link to the ignored env file. as to not be included in github push!
 var yelp = require("yelp").createClient({
-  consumer_key: "lA736NP1osOfTXudrMTfZw", 
-  consumer_secret: "IW4ne8VDMbrWGSkPak9z2AZK_sk",
-  token: "wvOx_MpAa1P6oBK4ASq-IlfzHgqFV5Lf",
-  token_secret: "Kmi79-x8Ldc36uAwNM8SOnEuFm8"
+  consumer_key: process.env.Dog_Key, 
+  consumer_secret: process.env.Dog_Secret,
+  token: process.env.Dog_Token,
+  token_secret: process.env.Dog_Secret_Token
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:false}));
+
+// var db = require('./models')
+
+
+app.get('/', function(req, res) {
+	res.render('index')
 });
 
 
-
+app.get('/search', function(req, res) {
+	yelp.search({term:"dog friendly", category_filter: "hotels,parks", location: "Seattle"}, function(error, data) {
+	console.log(error);
+	console.log(data);
+	res.send(data);	
+	});
+	
+})
 // See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({term: "dog friendly", location: "Seattle"}, function(error, data) {
-  console.log(error);
-  console.log(data);
-});
+
+
+app.listen(3000);
